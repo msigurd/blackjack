a7d65a6806919e13afbfb3488b74c4a0171118fd
msigurd <101131709+msigurd@users.noreply.github.com>
2026-01-27 19:13:24 +0100

    Draw dealer cards until higher than player hand

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function dealerShouldDrawCards() {
-    return !dealerHand.isBust && dealerHand < playerHand && dealerHand.cards.length < MAX_CARDS_IN_DECK
+    return !dealerHand.isBust && dealerHand <= playerHand && dealerHand.cards.length < MAX_CARDS_IN_DECK
              && !dealerHand.hasHardSeventeen && !playerHand.winsByFiveCardCharlieRule && !playerHand.hasBlackjack;
   }
 
